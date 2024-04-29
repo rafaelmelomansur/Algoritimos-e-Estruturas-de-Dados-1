eaabb8f95b4b3dc6b9b23ab57202b29a5cfa43ed
rafaelmelomansur <rafael.mansur2166@gmail.com>
2024-04-29 11:15:28 -0300

    Add files via upload

---
+++ prj_arrayEX1/src/prj_arrayEX1.c
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name        : prj_arrayEX1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int numeros [5];
+	int soma = 0;
+
+	printf("Digite 5 números inteiros:\n");
+
+	for (int i = 0; i < 5; i++) {
+		printf("Número %d: ", i + 1);
+		fflush(stdout);
+		scanf("%d", &numeros [i]);
+		soma = soma + numeros [i];
+	}
+
+	double media = (double) soma / 5;
+
+	printf("Soma: %d\n", soma);
+	printf("Média: %.2lf\n", media);
+
+	return 0;
+}
